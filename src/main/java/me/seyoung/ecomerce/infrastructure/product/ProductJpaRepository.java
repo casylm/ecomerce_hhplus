5ab97827aba7d1c7ea6b618b914c04d8af31fb5e
casylm <160497402+casylm@users.noreply.github.com>
2025-11-14 02:31:05 +0900

    refactor : product 기능개선

--- src/main/java/me/seyoung/ecomerce/infrastructure/product/ProductJpaRepository.java
+++ src/main/java/me/seyoung/ecomerce/infrastructure/product/ProductJpaRepository.java
@@ -3,9 +3,6 @@ package me.seyoung.ecomerce.infrastructure.product;
 import me.seyoung.ecomerce.domain.product.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-import java.util.Optional;
-
 public interface ProductJpaRepository extends JpaRepository<Product, Long> {
     // 상품 조회는 JpaRepository에서 제공
     // Optional<Product> findById(Long productId);
