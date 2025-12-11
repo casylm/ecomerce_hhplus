483884927affbba8be83ff4e7a51f9eb0a731e77
casylm <160497402+casylm@users.noreply.github.com>
2025-12-11 23:44:20 +0900

    feat : 설정 파일

---
+++ src/main/java/me/seyoung/ecomerce/config/RestTemplateConfig.java
@@ -0,0 +1,23 @@
+package me.seyoung.ecomerce.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+
+/**
+ * RestTemplate 설정
+ */
+@Configuration
+public class RestTemplateConfig {
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder
+                .setConnectTimeout(Duration.ofSeconds(5))
+                .setReadTimeout(Duration.ofSeconds(5))
+                .build();
+    }
+}
